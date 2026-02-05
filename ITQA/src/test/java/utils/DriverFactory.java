76a9b44375c3cc08b461bcbe6af7bb2ab5cdd5a5
watagalawgbn <watagalawgbn.21@uom.lk>
2026-02-05 13:25:26 +0530

    fixed google password manager popup issue

--- ITQA/src/test/java/utils/DriverFactory.java
+++ ITQA/src/test/java/utils/DriverFactory.java
@@ -4,6 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.Map;
+
+
 import java.time.Duration;
 
 public class DriverFactory {
@@ -12,13 +17,32 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Disable password manager and breach detection
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("credentials_enable_service", false);
+            prefs.put("profile.password_manager_enabled", false);
+            prefs.put("profile.password_manager_leak_detection", false);
+
+            options.setExperimentalOption("prefs", prefs);
+
+            // Extra safety flags
+            options.addArguments("--disable-save-password-bubble");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-infobars");
+            options.addArguments("--disable-extensions");
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver;
     }
+
 
     public static void quitDriver() {
         if (driver != null) {
@@ -26,4 +50,5 @@ public class DriverFactory {
             driver = null;
         }
     }
+
 }
